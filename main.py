9c23fa91037b7052a02fc8ef45dfdd9a8d2d8f32
Kyryl <krupartea@gmail.com>
2022-03-10 18:06:36 +0200

    Add salience score

--- main.py
+++ main.py
@@ -3,19 +3,43 @@ import torchvision
 import os
 import torch
 
+device = 'cuda'
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-model.eval()
+model.eval().to(device)
 
-img = cv2.imread(filename=r'man_with_cup.jpg')
-img_input = torch.tensor(img).movedim(-1, 0).unsqueeze(dim=0) / 255
-with torch.no_grad():
-    output = model(img_input)[0]
-    
-treshold_mask = (output['scores'] > 0.3)
-boxes = torch.reshape(torch.masked_select(output['boxes'], treshold_mask.unsqueeze(1)), (-1, 4)).type(torch.int32).cpu().numpy()
-for box in boxes:
-    pt1 = (box[0], box[1])
-    pt2 = (box[2], box[3])
-    cv2.rectangle(img, pt1, pt2, color=(0, 255, 0))
-cv2.imwrite('test.jpg', img)
-pass
+
+def rect_area(x1, y1, x2, y2):
+    return abs(x1-x2) * abs(y1-y2)
+
+
+
+def get_saliency(img):
+    # TODO: add scene clustering
+    with torch.no_grad():
+        output = model(img)[0]
+    boxes = output['boxes']
+    scores = output['scores']
+        
+    saliency = 0
+    for box, score in zip(boxes, scores):
+        saliency += score * rect_area(*box)
+        
+    return saliency
+
+
+def main():
+
+    # loop through all frames
+    FOLDER_PATH = 'frames'
+    scores = {}
+    for filename in os.listdir(FOLDER_PATH):
+        if filename.endswith('.jpg'):
+            file_path = os.path.join(FOLDER_PATH, filename)
+            img = cv2.imread(file_path)
+            img = torch.tensor(img).to(device).movedim(-1, 0).unsqueeze(dim=0) / 255
+            scores[filename] = get_saliency(img)
+    scores = dict(sorted(scores.items(), key=lambda item: item[1]))
+    pass
+
+if __name__ == '__main__':
+    main()
